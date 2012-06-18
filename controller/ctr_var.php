7a1aaf11630f5b2ef935a718d1b15350a1153cd9
Agus Prasetyo <agusprasetyo811@gmail.com>
2012-06-18 18:34:37 +0700

    ctr_var not work

---
+++ controller/ctr_var.php
@@ -0,0 +1,13 @@
+<?php 
+/*------------------------------ 
+Templates generator by Agus Prasetyo 
+email: agusprasetyo811@gmail.com 
+--------------------------------*/
+
+/* Menentukan variable untuk dijadikan parameter template contoh : "BODY_CENTER" => @$BODY_CENTER, SIDEBAR_SIDE" => @$SIDEBAR_SIDE  */
+
+$set_var = array(
+
+  BODY_CENTER" => @$BODY_CENTER,
+
+);
